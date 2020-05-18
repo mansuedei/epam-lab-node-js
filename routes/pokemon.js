f4d0ebdfcd4fdeaa89e0b3edf879e88ab7b99fc5
Iana Stepanova <Iana_Stepanova@epam.com>
2020-05-18 22:50:44 +0300

    setup server, connected to mongo, implemented CRUD

---
+++ routes/pokemon.js
@@ -0,0 +1,87 @@
+const express = require('express');
+const Pokemon = require('../models/pokemon-model');
+
+const router = express.Router();
+
+// Middleware
+async function getPokemon(req, res, next) {
+  let pokemon;
+  try {
+    pokemon = await Pokemon.findById(req.params.id);
+    if (pokemon == null) {
+      return res.status(404).json({ message: 'Cannot find this pokemon' });
+    }
+  } catch (error) {
+    return res.status(500).json({ message: error.message });
+  }
+
+  res.pokemon = pokemon;
+  next();
+}
+
+// Create one
+router.post('/', async (req, res) => {
+  const pokemon = new Pokemon({
+    name: req.body.name,
+    id: req.body.id,
+    caught: req.body.caught,
+    damage: req.body.damage,
+  });
+
+  try {
+    const newPokemon = await pokemon.save();
+    res.status(201).json(newPokemon);
+  } catch (error) {
+    res.status(400).json({ message: error.message });
+  }
+});
+
+// Read (getting all)
+router.get('/', async (req, res) => {
+  try {
+    const pokemon = await Pokemon.find();
+    res.json(pokemon);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+});
+
+// Read (getting one)
+router.get('/:id', getPokemon, (req, res) => {
+  res.json(res.pokemon);
+});
+
+// Update one
+router.patch('/:id', getPokemon, async (req, res) => {
+  if (req.body.name != null) {
+    res.pokemon.name = req.body.name;
+  }
+  if (req.body.id != null) {
+    res.pokemon.id = req.body.id;
+  }
+  if (req.body.damage != null) {
+    res.pokemon.damage = req.body.damage;
+  }
+  if (req.body.caught != null) {
+    res.pokemon.caught = req.body.caught;
+  }
+
+  try {
+    const updatedPokemon = await res.pokemon.save();
+    res.json(updatedPokemon);
+  } catch (error) {
+    res.status(400).json({ message: error.message });
+  }
+});
+
+// Delete one
+router.delete('/:id', getPokemon, async (req, res) => {
+  try {
+    await res.pokemon.remove();
+    res.json({ message: 'Deleted Pokemon' });
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+});
+
+module.exports = router;
