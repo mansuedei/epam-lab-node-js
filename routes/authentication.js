9a48ba3f773d620946847553e94a8c56c2f3b376
Iana Stepanova <Iana_Stepanova@epam.com>
2020-06-01 00:39:21 +0300

    implemented user registration, password hashed with bcrypt

--- routes/authentication.js
+++ routes/authentication.js
@@ -1,6 +1,9 @@
 const express = require('express');
+const bcrypt = require('bcrypt');
+const Users = require('../models/users-model');
 
 const authentication = express();
+const localUsersStorage = [];
 
 authentication.set('view-engine', 'ejs');
 authentication.use(express.urlencoded({ extended: false }));
@@ -19,8 +22,57 @@ authentication.get('/register', (req, res) => {
   res.render('register.ejs');
 });
 
-authentication.post('/register', (req, res) => {
-	req.body.name
+// Middleware
+async function getUser(req, res, next) {
+  let user;
+  try {
+    user = await Users.findById(req.params.id);
+    if (user == null) {
+      return res.status(404).json({ message: 'Cannot find this user' });
+    }
+  } catch (error) {
+    return res.status(500).json({ message: error.message });
+  }
+
+  res.user = user;
+  next();
+}
+
+// Create one
+authentication.post('/register', async (req, res) => {
+  try {
+    const hashedPassword = await bcrypt.hash(req.body.password, 10);
+    const user = new Users({
+      name: req.body.name,
+      email: req.body.email,
+      password: hashedPassword,
+    });
+
+    await user.save();
+    res.redirect('/login');
+  } catch (error) {
+    res.redirect('/register');
+  }
+});
+
+// Read (getting all)
+authentication.get('/users', async (req, res) => {
+  try {
+    const users = await Users.find();
+    res.json(users);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+});
+
+// Delete one
+authentication.delete('/:id', getUser, async (req, res) => {
+  try {
+    await res.user.remove();
+    res.json({ message: 'Deleted the user' });
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
 });
 
 module.exports = authentication;
